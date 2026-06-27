45fc88186768b5662788daba831dd41c46a5d9c4
TheVidz <yovidit2004@gmail.com>
2026-06-27 16:31:05 +0530

    rename to fl-from-flwr

--- fl-from-flwr/model.py
+++ fl-from-flwr/model.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 SUPERPOPS = ["AFR", "AMR", "EAS", "EUR", "SAS"]
 
 class AncestryNet(nn.Module):
-    def __init__(self, input_dim=10, num_classes=5):  # Crucial: Must be 10 everywhere!
+    def __init__(self, input_dim=10, num_classes=5):  # 10 principal components!
         super(AncestryNet, self).__init__()
         self.fc_layer = nn.Sequential(
             nn.Linear(input_dim, 32),
