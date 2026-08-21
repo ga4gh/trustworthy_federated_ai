b98d2f830b772a8d6e759bb434da8109b6dcd10c
TheVidz <yovidit2004@gmail.com>
2026-08-21 16:57:48 +0530

    fix: change default data dir for splits

--- scripts/prepare_splits_after_flan.py
+++ scripts/prepare_splits_after_flan.py
@@ -81,7 +81,7 @@ def main():
     parser.add_argument("--num-sites", type=int, default=4, help="Number of client sites to generate.")
     parser.add_argument("--fold", type=int, default=0, help="Which fold to use from the cache.")
     parser.add_argument("--cache-dir", type=str, default=str(Path.home() / '.cache' / 'trustworthy_fed_ai'), help="Path to the global prepared data cache.")
-    parser.add_argument("--out-dir", type=str, default="../data", help="Where to save the output TSVs for the DRS seeder.")
+    parser.add_argument("--out-dir", type=str, default="./data", help="Where to save the output TSVs for the DRS seeder.")
     args = parser.parse_args()
 
     POPULATION_TO_SUPERPOP = {
